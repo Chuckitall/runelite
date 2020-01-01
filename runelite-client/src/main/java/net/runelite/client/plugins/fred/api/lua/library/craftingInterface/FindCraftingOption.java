0af22ed3302ab9c7a2a5faaa538814a2e541f759
Nathaniel <natruf@gmail.com>
2020-01-01 12:21:41 -0600

    implementing crafting interface lua lib

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/fred/api/lua/library/craftingInterface/FindCraftingOption.java
@@ -0,0 +1,22 @@
+package net.runelite.client.plugins.fred.api.lua.library.craftingInterface;
+
+
+import net.runelite.api.QueryResults;
+import net.runelite.api.queries.DialogQuery;
+import net.runelite.client.RuneLite;
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.OneArgFunction;
+import org.luaj.vm2.lib.jse.CoerceJavaToLua;
+
+public class FindDialog extends OneArgFunction
+{
+	@Override
+	public LuaValue call(LuaValue arg)
+	{
+		if (!arg.isstring())
+		{
+			return argerror(1, "Must be the text to search for.");
+		}
+		return RuneLite.getClient().map(f -> new DialogQuery().textContains(arg.checkjstring()).result(f)).filter(f -> f.size() > 0).map(QueryResults::first).map(CoerceJavaToLua::coerce).orElse(NIL);
+	}
+}

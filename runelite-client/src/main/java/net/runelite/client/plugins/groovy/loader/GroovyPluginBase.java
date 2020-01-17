4987c6d5e6bd573eb60c6d43116c61c59b88a6fb
Fred4106 <natruf@gmail.com>
2020-01-17 15:20:31 -0600

    not sure what I'm doing...

--- runelite-client/src/main/java/net/runelite/client/plugins/groovy/loader/GroovyPluginBase.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/groovy/loader/GroovyPluginBase.java
@@ -1,24 +1,22 @@
 package net.runelite.client.plugins.groovy.loader;
 
-import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.client.eventbus.EventBus;
 import net.runelite.client.menus.MenuManager;
 import net.runelite.client.ui.overlay.OverlayManager;
 
-@Slf4j
 public abstract class GroovyPluginBase
 {
-	public Client client;
-	public EventBus eventBus;
-	public MenuManager menuManager;
-	public OverlayManager overlayManager;
+	public Client _client;
+	public EventBus _eventBus;
+	public MenuManager _menuManager;
+	public OverlayManager _overlayManager;
 	public GroovyPluginBase(ScriptContext ctx)
 	{
-		client = ctx.getClient();
-		eventBus = ctx.getEventBus();
-		menuManager = ctx.getMenuManager();
-		overlayManager = ctx.getOverlayManager();
+		_client = ctx.getClient();
+		_eventBus = ctx.getEventBus();
+		_menuManager = ctx.getMenuManager();
+		_overlayManager = ctx.getOverlayManager();
 	}
 
 	public abstract void startup();

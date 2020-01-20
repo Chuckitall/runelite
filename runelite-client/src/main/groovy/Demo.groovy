92b1ace9b557a1b806ab87372b3d27421534948a
Fred4106 <natruf@gmail.com>
2020-01-20 17:39:22 -0600

    demo can debug RunScriptEvents now.

--- runelite-client/src/main/groovy/Demo.groovy
+++ runelite-client/src/main/groovy/Demo.groovy
@@ -4,6 +4,7 @@ import io.reactivex.functions.Consumer
 import net.runelite.api.VarClientInt
 import net.runelite.api.VarClientStr
 import net.runelite.api.events.GameTick
+import net.runelite.api.events.RunScriptEvent
 import net.runelite.api.widgets.Widget;
 import net.runelite.client.plugins.groovy.debugger.DebuggerWindow.LogLevel
 import net.runelite.client.plugins.groovy.script.ScriptedPlugin
@@ -11,25 +12,14 @@ import net.runelite.client.plugins.groovy.script.ScriptedPlugin
 @CompileStatic
 @InheritConstructors
 class Demo extends ScriptedPlugin {
-	void onTick(GameTick e)
+	void onScriptRunEvent(RunScriptEvent e)
 	{
-		Widget widget = _client.getWidget(162, 45);
-
-		if (widget != null) {
-			_client.setVar(VarClientInt.INPUT_TYPE, 7);
-			_client.setVar(VarClientStr.INPUT_TEXT, String.valueOf(4));
-			_clientThread.invoke(() -> {this._client.runScript(681, null)});
-			_clientThread.invoke(() -> {this._client.runScript(299, 0, 0)});
-		}
-	}
-
-	void onMenuEntryClicked()
-	{
-
+		log(LogLevel.INFO, "Script called with id: " + e.scriptId + " and arguments " + e.arguments.toString());
+		//log(LogLevel.INFO, e.getTarget().toLowerCase().contains("battlestaff"));
 	}
 
 	void startup() {
-		_eventBus.subscribe(GameTick.class, this, this.&onTick as Consumer<GameTick>);
+		_eventBus.subscribe(RunScriptEvent.class, this, this.&onScriptRunEvent as Consumer<RunScriptEvent>);
 		log(LogLevel.DEBUG, "Starting Up");
 	}
 
